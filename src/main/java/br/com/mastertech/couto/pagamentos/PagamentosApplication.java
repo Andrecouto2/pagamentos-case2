e0589ea183922875102cd8022186edf172dc7b93
andrecouto <coutoandreluis@gmail.com>
2020-08-17 09:02:45 -0300

    added ribbon config

--- src/main/java/br/com/mastertech/couto/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/mastertech/couto/pagamentos/PagamentosApplication.java
@@ -1,13 +1,16 @@
 package br.com.mastertech.couto.pagamentos;
 
+import br.com.mastertech.couto.pagamentos.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class PagamentosApplication {
 
     public static void main(String[] args) {
